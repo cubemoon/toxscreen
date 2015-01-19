af06a30c41f1b4186ab0cacc36691d3d5aa0555f
saneki <fake.saneki@gmail.com>
2015-01-18 20:00:11 -0600

    Status bar label

--- src/forms/mainwindow.cpp
+++ src/forms/mainwindow.cpp
@@ -2,24 +2,33 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+// Wait 3 seconds before clearing status
+int MainWindow::clearStatusMilli = 3000;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
-    pTox(nullptr),
-    pPrimaryMenu(nullptr)
+    pStatusClearTimer(new QTimer(this))
 {
     ui->setupUi(this);
 
     initButtons();
     initMenus();
+    initStatusBar();
+    initStatusClearTimer();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
     delete pPrimaryMenu;
+    delete pStatusLabel;
+    delete pStatusClearTimer;
 }
 
+/**
+ * Initialize QPushButton menus.
+ */
 void MainWindow::initMenus()
 {
     pPrimaryMenu = new QMenu("ToxScreen");
@@ -33,12 +42,58 @@ void MainWindow::initMenus()
     ui->toxScreenMenuPushButton->setMenu(pPrimaryMenu);
 }
 
+/**
+ * Initialize button connections.
+ */
 void MainWindow::initButtons()
 {
     QObject::connect(ui->passwordRefreshPushButton, &QPushButton::clicked, this, &MainWindow::onPasswordRefreshButtonClicked);
     QObject::connect(ui->toxIdCopyPushButton, &QPushButton::clicked, this, &MainWindow::onToxIdCopyButtonClicked);
 }
 
+/**
+ * Initialize the status bar. This is necessary as QtCreator doesn't allow
+ * for any children to be added to a QStatusBar.
+ */
+void MainWindow::initStatusBar()
+{
+    pStatusLabel = new QLabel("");
+    pStatusLabel->setObjectName("statusLabel");
+    //pStatusLabel->setAlignment(Qt::AlignRight);
+    ui->statusBar->addWidget(pStatusLabel);
+}
+
+/**
+ * Initialize the status clear QTimer.
+ */
+void MainWindow::initStatusClearTimer()
+{
+    pStatusClearTimer->setInterval(MainWindow::clearStatusMilli);
+    QObject::connect(pStatusClearTimer, &QTimer::timeout, this, &MainWindow::clearStatus);
+}
+
+/**
+ * Clear the status label.
+ */
+void MainWindow::clearStatus()
+{
+    setStatus("");
+}
+
+/**
+ * Set the status label text.
+ * @param status Status label text
+ */
+void MainWindow::setStatus(const QString &status)
+{
+    pStatusLabel->setText(status);
+    pStatusClearTimer->start();
+}
+
+/**
+ * Set the associated QTox instance.
+ * @param tox Pointer to QTox
+ */
 void MainWindow::setTox(QTox *tox)
 {
     pTox = tox;
@@ -49,24 +104,45 @@ void MainWindow::setTox(QTox *tox)
     ui->toxIdLineEdit->setCursorPosition(0);
 }
 
+/**
+ * Called when the Connect menu action is triggered.
+ * @param checked Checked, unused
+ */
 void MainWindow::onConnectAction(bool checked)
 {
     qDebug("Connect action triggered");
 }
 
+/**
+ * Called when the Exit menu action is triggered. Will close
+ * this window.
+ * @param checked Checked, unused
+ */
 void MainWindow::onExitAction(bool checked)
 {
     close();
 }
 
+/**
+ * Called when the password refresh button is clicked. Will
+ * regenerate a new password.
+ * @param checked Checked, unused
+ */
 void MainWindow::onPasswordRefreshButtonClicked(bool checked)
 {
     QString newPass = mPasswordGenerator.generate();
     ui->passwordLineEdit->setText(newPass);
+    setStatus("Refreshed password");
 }
 
+/**
+ * Called when the Tox Id copy button is clicked. Will copy the
+ * Tox Id into the clipboard.
+ * @param checked Checked, unused
+ */
 void MainWindow::onToxIdCopyButtonClicked(bool checked)
 {
     QClipboard *clipboard = QApplication::clipboard();
     clipboard->setText(pTox->getAddress().toHex().toUpper());
+    setStatus("Copied Tox Id to clipboard");
 }
--- src/forms/mainwindow.h
+++ src/forms/mainwindow.h
@@ -1,8 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLabel>
 #include <QMainWindow>
 #include <QMenu>
+#include <QString>
+#include <QTimer>
 #include "../qt/QTox.h"
 #include "../password_generator.h"
 
@@ -17,20 +20,27 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void setStatus(const QString &status);
     void setTox(QTox *tox);
 
 private:
+    void clearStatus();
     void initButtons();
     void initMenus();
+    void initStatusBar();
+    void initStatusClearTimer();
     void onConnectAction(bool checked);
     void onExitAction(bool checked);
     void onPasswordRefreshButtonClicked(bool checked);
     void onToxIdCopyButtonClicked(bool checked);
     Ui::MainWindow *ui;
+    QTimer *pStatusClearTimer;
     PasswordGenerator mPasswordGenerator;
     QTox *pTox;
     QMenu *pPrimaryMenu;
+    QLabel *pStatusLabel;
 
+    static int clearStatusMilli;
 };
 
 #endif // MAINWINDOW_H
